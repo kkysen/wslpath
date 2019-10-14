67c3e4b739fe91a1b3ef137da55a96081e243eb7
Khyber Sen <kkysen@gmail.com>
2019-10-14 03:01:35 -0400

    Got it to work in most normal cases.  Boundary cases like c/ when cwd is /mnt, or ../mnt/c don't work.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use wslpath::wsl_to_windows_path;
+use std::path::Path;
+
+fn run() -> Option<()> {
+    let arg = std::env::args().nth(1)?;
+    let path = Path::new(arg.as_str());
+    let path = wsl_to_windows_path(path).ok()?;
+    let path = path.expect("not on Windows");
+    let path = path.to_str()?;
+    println!("{}", path);
+    Some(())
+}
+
 fn main() {
-    println!("Hello, World!");
+    run().unwrap();
 }
