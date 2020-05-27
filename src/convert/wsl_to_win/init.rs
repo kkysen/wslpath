75ee6d523a0f11b2a8bf7c16abf392e77fa7885d
Khyber Sen <kkysen@gmail.com>
2020-05-27 00:59:02 -0400

    Added canonicalization support for wsl_to_win, but the actual conversion isn't done yet.

--- src/convert/wsl_to_win/init.rs
+++ src/convert/wsl_to_win/init.rs
@@ -1,6 +1,38 @@
 use crate::convert::path_sep::WindowsPathSep;
+use crate::convert::wsl::get_unc_root;
+use std::path::PathBuf;
 
 pub struct Options {
     pub sep: WindowsPathSep,
     pub canonicalize: bool,
 }
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            sep: WindowsPathSep::default(),
+            canonicalize: true,
+        }
+    }
+}
+
+pub struct Root {
+    pub unc: PathBuf,
+}
+
+#[derive(Error, Debug)]
+pub enum ConvertOptionsError {
+    #[error("not running on WSL")]
+    NotWsl,
+}
+
+impl Root {
+    pub fn new(_options: &Options) -> Result<Self, ConvertOptionsError> {
+        Ok(Self {
+            unc: get_unc_root()
+                .map(PathBuf::from)
+                .ok_or(ConvertOptionsError::NotWsl)?,
+        })
+    }
+}
+
--- src/convert/wsl_to_win/mod.rs
+++ src/convert/wsl_to_win/mod.rs
@@ -1,28 +1,50 @@
 use thiserror::Error;
 
 pub use crate::convert::wsl_to_win::init::Options;
+use crate::convert::wsl_to_win::init::{ConvertOptionsError, Root};
+use std::{fs, io};
+use std::path::Path;
+use std::ffi::OsStr;
 
 mod init;
 mod encode;
 
 #[derive(Error, Debug)]
-pub enum ConvertOptionsError {}
+pub enum ConvertError {
+    #[error("failed to canonicalize path")]
+    CanonicalizationFailed(#[from] io::Error),
+}
 
-#[derive(Error, Debug)]
-pub enum ConvertError {}
-
-pub struct Converter {}
+pub struct Converter {
+    options: Options,
+    root: Root,
+}
 
 impl super::Converter for Converter {
     type Options = Options;
     type OptionsError = ConvertOptionsError;
     type Error = ConvertError;
     
-    fn new(_options: Self::Options) -> Result<Self, Self::OptionsError> {
-        unimplemented!()
+    fn new(options: Self::Options) -> Result<Self, Self::OptionsError> {
+        let root = Root::new(&options)?;
+        Ok(Self {
+            options,
+            root,
+        })
     }
     
-    fn convert_into_buf(&self, _path: &mut [u8], _buf: &mut Vec<u8>) -> Result<(), Self::Error> {
+    fn convert_into_buf(&self, path: &mut [u8], _buf: &mut Vec<u8>) -> Result<(), Self::Error> {
+        let _path = if self.options.canonicalize {
+            Ok(path)
+                .map(OsStr::from_bytes)
+                .map(Path::new)
+                .and_then(fs::canonicalize)?
+                .into_os_string()
+                .into_vec()
+                .as_mut_slice()
+        } else {
+            path
+        };
         unimplemented!()
     }
 }
